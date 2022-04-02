f0dcdd0778fd08813db09550a1a741fa1ef86b97
Mugen <melton.sheridan@gmail.com>
2022-04-02 04:11:05 -0400

    added javascript functionality

---
+++ Develop/script.js
@@ -0,0 +1,54 @@
+// Current date
+let currentDate = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    // event listener for save button
+    $(".saveBtn").on("click", function () {
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+    function timeSaver() {
+        let timeNow = moment().hour();
+
+        $(".time-block").each(function () {
+            let blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("present");
+
+            }
+
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeSaver();
+})
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Work Day Scheduler Starter Code
+
+# Description
+a calendar app that allows the user to make and save plans from 9am to 5pm. This app uses the browser to function is ran by Javascript and JjQuery.
+
+This is an assignment for the Butler Coding Bootcamp
